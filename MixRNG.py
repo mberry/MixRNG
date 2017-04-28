4d3c728b58c98062b493b9960bd55bde71658158
Mitchell <berry.mitch@gmail.com>
2017-04-28 11:05:55 +1000

    Update MixRNG.py

--- MixRNG.py
+++ MixRNG.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
+import time
 import serial
 from os import urandom
-from time import sleep
 
 
-def hwrng(numbytes, port='COM4'):
+def extrng(numbytes, port='COM4'):
     """Extracts bits from OneRNG, check serial settings and start/stop command if using different hardware"""
     ser = serial.Serial(
         port=port,
@@ -15,23 +15,28 @@ def hwrng(numbytes, port='COM4'):
         timeout=5,
         rtscts=True)
 
-    # Wait for handshake, Request To Send True
-    sleep(0.5)
+    # Wait for handshake, Request To Send set True
+    time.sleep(0.2)
     ser.setRTS(1)
 
-    # Entropy feed start/stops on input of cmdO/cmdo for the OneRNG.
+    # Feed start/stops on input of cmdO/cmdo for OneRNG hardware
     ser.write(bytes('cmdO'.encode('ascii'))) # Start
-    sleep(0.1)
+    time.sleep(0.1)
+    
+    # Read
     hwrng = ser.read(numbytes)
-    sleep(0.1)
-    ser.write(bytes('cmdo'.encode('ascii'))) # Stop
+    time.sleep(0.1)
+    
+    # Stop
+    ser.write(bytes('cmdo'.encode('ascii'))) 
     ser.setRTS(0)
     ser.close()
+    
     return hwrng
 
 
 def xorbytes(a, b):
-    """ bytes >> int >> xor >> int >> bytes """
+    """ bytes > int > xor > int > bytes """
     a = int.from_bytes(a, byteorder='big')
     b = int.from_bytes(b, byteorder='big')
     xor = a ^ b
@@ -40,7 +45,7 @@ def xorbytes(a, b):
 
 def mixrng(numbytes, port='COM4'):
     """Returns bitwise xor of an inbuilt and hardware CSRNG"""
-    ib = urandom(numbytes)
-    hw = hwrng(numbytes, port)
-    return xorbytes(ib, hw)
+    internal = urandom(numbytes)
+    external = extrng(numbytes, port)
+    return xorbytes(internal, external)
 
